d95024784769630d88a9a966bc625682f060aaec
Илья Грачев <grachev.ia96@gmail.com>
2022-06-06 23:23:43 +0700

    added src

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        StreamMain streammain = new StreamMain();
+
+        streammain.main();
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int x : intList) {
+            if (x > 0 && x % 2 == 0) {
+                result.add(x);
+            }
+        }
+        Collections.sort(result);
+
+        System.out.println(result);
+
+
+
+
+
+    }
+
+}
